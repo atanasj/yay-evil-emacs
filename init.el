7aa365df11c117d2e941eabc0f9569363d221406
atanasj <atanas.atanas@gmail.com>
2020-01-16 01:52:04 +1100

    removed custom variables, moved them to atanas.org

--- init.el
+++ init.el
@@ -49,17 +49,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (ace-window yasnippet-snippets yaml-mode which-key restart-emacs rainbow-delimiters quelpa-use-package projectile poly-R paren-face pandoc-mode org-bullets ob-applescript multiple-cursors json-mode ido-vertical-mode ido-completing-read+ hl-todo highlight-operators highlight-numbers highlight-escape-sequences general font-lock+ flycheck flx-ido exec-path-from-shell evil-magit evil-commentary evil-collection ess doom-themes doom-modeline dired-subtree dired-rainbow diminish dashboard company auto-package-update aggressive-indent ag))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
